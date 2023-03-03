930f5b09abc20dc18f65eab7699dca8ad7317c1e
Build Er <builder@macmini6.local>
2023-03-03 06:33:23 -0300

    Package version: 1.0.0-beta+20230303092649

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_ContactsWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Contacts",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS, .watchOS])),
 					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Contacts",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Contacts-1.0.0-beta+20230302113401.xcframework.zip",
-			checksum: "039ac09288dddcf9e205570e0a2b17099751973aa03a325205d5209fa90793a2"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Contacts-1.0.0-beta+20230303092649.xcframework.zip",
+			checksum: "7838838c4d22b060b9ce391a37ab0ef0e23713412626f7617de9d1dd41637499"
 		)
 	]
 )
