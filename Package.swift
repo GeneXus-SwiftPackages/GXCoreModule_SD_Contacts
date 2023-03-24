7f00e51a6c604b50f1f7585cb60cebef4c4500b4
genexusbot <builder@genexus.com>
2023-03-24 12:46:18 -0300

    Package version: 1.0.0-beta+20230324153906

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_ContactsWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Contacts",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS, .watchOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .iOS, .tvOS])),
 					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Contacts",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Contacts-1.0.0-beta+20230324150340.xcframework.zip",
-			checksum: "c5fd0da500c715151f59b981d0b8690dec1f47eea0e3bc1a77f19baf1ba8d265"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Contacts-1.0.0-beta+20230324153906.xcframework.zip",
+			checksum: "b163dc3315b03896906b6fe3e385f4873eef547a25fef58b9195edbc84f5c21d"
 		)
 	]
 )
